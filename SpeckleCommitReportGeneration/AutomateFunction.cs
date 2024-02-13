4b75a62cfb24f592c5ce76a6f6b5b4d6d0d4bdfc
Chris Welch <chris.welch@aurecongroup.com>
2024-02-14 12:59:51 +1300

    added a report artifact

--- SpeckleCommitReportGeneration/AutomateFunction.cs
+++ SpeckleCommitReportGeneration/AutomateFunction.cs
@@ -3,27 +3,63 @@ using Objects.Geometry;
 using Speckle.Automate.Sdk;
 using Speckle.Core.Logging;
 using Speckle.Core.Models.Extensions;
+using ClosedXML;
+using ClosedXML.Excel;
+using System.Data;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 static class AutomateFunction
 {
-  public static async Task Run(
-    AutomationContext automationContext,
-    FunctionInputs functionInputs
-  )
-  {
-    Console.WriteLine("Starting execution");
-    _ = typeof(ObjectsKit).Assembly; // INFO: Force objects kit to initialize
+	public static async Task Run(AutomationContext automationContext, FunctionInputs functionInputs)
+	{
+		Console.WriteLine("Starting execution");
+		_ = typeof(ObjectsKit).Assembly; // INFO: Force objects kit to initialize
 
-    Console.WriteLine("Receiving version");
-    var commitObject = await automationContext.ReceiveVersion();
+		Console.WriteLine("Receiving version");
+		var commitObject = await automationContext.ReceiveVersion();
 
-    Console.WriteLine("Received version: " + commitObject);
+		Console.WriteLine("Received version: " + commitObject);
 
-    var count = commitObject
-      .Flatten()
-      .Count(b => b.speckle_type == functionInputs.SpeckleTypeToCount);
+		Console.WriteLine("Creating datatable");
+		var streamDataTable = new DataTable();
+		streamDataTable.TableName = "StreamObjects";
 
-    Console.WriteLine($"Counted {count} objects");
-    automationContext.MarkRunSuccess($"Counted {count} objects");
-  }
+		var columns = new[] { "Id", "Name", "Speckle Type" };
+
+		foreach ( var column in columns )
+		{
+			streamDataTable.Columns.Add(column, typeof(string));
+		}
+
+		foreach (var commit in commitObject.Flatten())
+		{
+			var row = streamDataTable.NewRow();
+
+			foreach (var column in columns)
+			{
+				var property = column.ToLowerInvariant().Replace(' ', '_');
+
+				row[column] = commit[property]?.ToString() ?? "";				
+			}
+			streamDataTable.Rows.Add(row);
+		}
+
+		var outputFile = $"out/Speckle Objects.xlsx";
+
+
+		Console.WriteLine("Creating workbook");
+		using (var workbook = new XLWorkbook())
+		{
+			var streams = workbook.Worksheets.Add(streamDataTable);
+
+			Console.WriteLine($"Saving workbook at '{Path.GetFullPath(outputFile)}'");
+			workbook.SaveAs(outputFile);
+		}
+
+		Console.WriteLine("Storing file");
+		await automationContext.StoreFileResult(outputFile);
+
+		automationContext.MarkRunSuccess($"Created report");
+	}
 }
