f804ae8e3282557f970347eafe897437a5ea88d4
Chris Welch <chris.welch@aurecongroup.com>
2024-02-14 14:08:47 +1300

    tada

--- SpeckleCommitReportGeneration/FunctionInputs.cs
+++ SpeckleCommitReportGeneration/FunctionInputs.cs
@@ -7,6 +7,6 @@ using System.ComponentModel.DataAnnotations;
 /// are valid and match the required schema.
 struct FunctionInputs
 {
-  //[Required]
-  //public string SpeckleTypeToCount;
+  [Required]
+  public string SpeckleTypeToCount;
 }
